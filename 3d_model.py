b26ed20abd3a26b14c0d71f25dd49719615da403
ddenunzi <69667822+ddenunzi@users.noreply.github.com>
2022-05-16 00:33:10 -0400

    Add files via upload

--- 3d_model.py
+++ 3d_model.py
@@ -6,18 +6,31 @@ import matplotlib.pyplot as plt
 from create_lists import still_targets_xyz,moving_targets_xyz,moving_cameras_xyz,still_cameras_xyz
 
 
-#CALLIBRATE CAMERA
+#CALIBRATE CAMERA
 cv.calibrateCamera()
 
 #distortion coeff
 #camera matrix
+cam_matrix=0
+dist_coeff=0
 
 
 #define coordinates 3D
+obj_coordinates_still=still_targets_xyz
+obj_coordinates_moving=moving_targets_xyz
+
+
+camera_coordinates_still=still_cameras_xyz
+camera_coordinates_moving=moving_cameras_xyz
 
 #find coordinates 2D
+    #i want to use the still target coordinates since they should be the most accurate and the position of the moving cameras.
 rotation_vector=[]
 translation_vector=[]
-obj_coordinates=still_targets_xyz
-camera_coordinates=moving_cameras_xyz
-projection_pixels=cv.projectPoints()
+
+projection_pixels=cv.projectPoints(obj_coordinates_still,rotation_vector,translation_vector,cam_matrix,dist_coeff)
+
+
+##how do i define where the camera is??????
+#https://python.hotexamples.com/examples/cv2/-/projectPoints/python-projectpoints-function-examples.html
+##https://github.com/radjkarl/imgProcessor/blob/master/imgProcessor/camera/PerspectiveCorrection.py
