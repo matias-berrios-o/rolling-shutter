2c31259a59b614eea304025e93f288b80dc9ba49
ddenunzi <69667822+ddenunzi@users.noreply.github.com>
2022-06-11 22:01:50 -0400

    Add files via upload

---
+++ scripts/create_lists.py
@@ -0,0 +1,282 @@
+
+import cv2 as cv
+
+
+
+PATH_1="scripts/files/camera_m_xyz_ang.txt"
+PATH_2="scripts/files/target_m_xyz.txt"
+PATH_3="scripts/files/camera_s_xyz_ang.txt"
+PATH_4="scripts/files/target_s_xyz.txt"
+PATH_5="scripts/files/pixels_m.txt"
+PATH_6="scripts/files/pixels_s.txt"
+
+PATH_7="photos/cropped_images"
+
+#FOR ANY TXT FILE
+#RECEIVES FILE PATH AND READS INFORMATION, CREATES LIST OF LISTS WITH INFORMATION
+#OUTPUT: UNFILTERED LIST OF LISTS
+def open_files(type,path):
+    data_file=[]
+    if type!="pixel":
+        with open(path, 'r') as file:
+            file_information=file.readlines()
+            for line in file_information:
+                line=line.strip()
+                line=line.split(",")
+                data_file.append(line)
+
+    else:
+        with open(path, 'r') as file:
+            file_information=file.readlines()
+            for line in file_information:
+                line=line.strip()
+                line=line.split("\t")
+                data_file.append(line)
+
+    return data_file
+
+#FOR ANY FILE
+#SEPARATES TITLES AND EXTRA INFORMATION CONTAINED IN FILE, CREATES LIST OF LISTS WITH TARGET NAME AND INFORMATION FROM FILE INFORMATION.
+#OUTPUT: FILTERED LIST OF LISTS [["target 1",float(x1),float(y1),float(z1)],["target 2",float(x2),float(y2),float(z2)],....["target n",float(xn),float(yn),float(zn)]]
+def filter_information(type,data_files):
+    data_=[]
+    if type!="pixel":
+        for line in data_files[2:]:
+            new_line=[]
+            for i in line:
+                if i!="":
+                    new_line.append(i)
+            data_.append(new_line)
+    else:
+        for line in data_files[1:]:
+            new_line=[line[0],"target "+str(line[1]),line[2],line[3]]
+            data_.append(new_line)
+
+    return data_
+
+#ONLY FOR TARGETS FROM 'STILL' FILES
+#CREATES LIST OF LISTS WITH TARGET NAME AND CORRESPONDING COORDINATES. FILTERS INFORMATION FROM REFERENCE TARGETS
+#CONSIDERATION: TARGETS THAT WERE USED AS REFERENCES MUST BE FILTERED TO GET CORRESPONDING COORDINATES
+#OUTPUT: FILTERED LIST OF LISTS [["target 1",float(x1),float(y1),float(z1)],["target 2",float(x2),float(y2),float(z2)],....["target n",float(xn),float(yn),float(zn)]]
+def targets_xyz_still(data_targets):
+    xy_estimates=[]
+    for target in data_targets:
+        if target[0]=='target 8' or target[0]=='target 51' or target[0]=='target 159' or target[0]=='target 59' :
+            new_target=[target[0],float(target[9]),float(target[10]),float(target[11])]
+            xy_estimates.append(new_target)
+
+        else:
+            target[1]=float(target[1])
+            target[2]=float(target[2])
+            target[3]=float(target[3])
+            xy_estimates.append(target)
+    return xy_estimates[:len(data_targets)-1]
+
+#ONLY FOR TARGETS FROM 'MOVING' FILES
+#CREATES LIST OF LISTS WITH TARGET NAME AND CORRESPONDING COORDINATES. FILTERS INFORMATION FROM REFERENCE TARGETS
+#CONSIDERATION: TARGETS THAT WERE USED AS REFERENCES MUST BE FILTERED TO GET CORRESPONDING COORDINATES
+#OUTPUT: FILTERED LIST OF LISTS [["target 1",float(x1),float(y1),float(z1)],["target 2",float(x2),float(y2),float(z2)],....["target n",float(xn),float(yn),float(zn)]]
+def targets_xyz_moving(data_targets):
+    xy_estimates=[]
+    for target in data_targets:
+        if target[0]=='target 8' or target[0]=='target 5' or target[0]=='target 4' or target[0]=='target 51' :
+            new_target=[target[0],float(target[9]),float(target[10]),float(target[11])]
+            xy_estimates.append(new_target)
+
+        else:
+            target[1]=float(target[1])
+            target[2]=float(target[2])
+            target[3]=float(target[3])
+            xy_estimates.append(target)
+    return xy_estimates[:len(data_targets)-1]
+
+#ONLY FOR TARGET FILES
+#RECEIVES A LIST OF LISTS OF TARGET COORDINATES WITH TARGET NAME, ELIMINATES THE TARGET NAME TO ONLY LEAVE THE TARGET COORDINATES
+#OUTPUT: LIST OF TUPLES [(float(x1),float(y1),float(z1)),(float(x2),float(y2),float(z2)),....(float(xn),float(yn),float(zn))]
+def only_coordinates(data):
+    final_coordinates=[]
+
+    for line in data:
+        coordinates=(float(line[1]),float(line[2]),float(line[3]))
+        final_coordinates.append(coordinates)
+
+    return final_coordinates
+
+#ONLY FOR CAMERA FILES
+#FILTERS CAMERA DATA OBTAINED FROM CAMERA FILES TO OBTAIN FILENAME, XYZ COORDINATES AND YAW PITCH ROLL COORDINATES
+#OUTPUT: LIST OF LISTS WITH TUPLES FOR ROTATION AND TRANSLATION VECTORS [["FILEPATH1",(float(x1),float(y1),float(z1)),(float(yaw1),float(pitch1),float(roll1))],....["FILEPATHn",(float(xn),float(yn),float(zn)),(float(yawn),float(pitchn),float(rolln))]]
+def camera_coordinates(data):
+    final_coordinates=[]
+
+    for line in data:
+        coordinates=[line[0],(float(line[1]),float(line[2]),float(line[3])),(float(line[4]),float(line[5]),float(line[6]))]
+        final_coordinates.append(coordinates)
+    return final_coordinates
+
+#ONLY FOR PIXEL FILES
+#FILTERS PIXEL DATA OBTAINED FROM PIXEL FILES TO OBTAIN FILENAME, TARGET NAME, X, Y
+#OUTPUT: LIST OF LISTS LIST OF LISTS [["FILEPATH1","target 1",float(x1),float(y1)],["FILEPATH1","target 2",float(x2),float(y2)],....["FILEPATHn","target n",float(xn),float(yn)]]
+def pixel_coordinate(data):
+    final_coordinates=[]
+
+    for line in data:
+        coordinates=[line[0],line[1],float(line[2]),float(line[3])]
+        final_coordinates.append(coordinates)
+    return final_coordinates
+
+#ONLY FOR CAMERA FILES
+#CREATES LIST WITH IMAGE PATHS, IMAGES PATHS ARE FOUND IN THE LIST CONTAINING CAMERA COORDINATES
+#OUTPUT: LIST OF FILEPATHS ["FOLDER/FILEPATH1","FOLDER/FILEPATH2",..."FOLDER/FILEPATHn"]
+def image_paths(folder,data):
+    paths=[]
+    for line in data:
+        filepath=folder+"/"+line[0]
+        paths.append(filepath)
+    return paths
+
+#ONLY FOR TARGET AND PIXEL FILES
+#FILTERS A COORDINATE LIST FOR TARGETS I WANT TO USE, GIVEN BY LIST "BOARD_TARGETS". "BOARD_TARGETS" IS SORTED TO BE IN ORDER BY TARGET NUMBER.
+#OUTPUT: LIST OF LISTS [["target 1",float(x1),float(y1),float(z1)],["target 2",float(x2),float(y2),float(z2)],....["target n",float(xn),float(yn),float(zn)]]
+def filter_points(type,data,board_targets):
+    new_board_coordinates=[]
+    if type!="pixel":
+
+        for dat in data:
+            for target in board_targets:
+                if dat[0]==target:
+                    new_board_coordinates.append(dat)
+
+    else:
+        for dat in data:
+            for target in board_targets:
+                if dat[1]==target:
+                    new_board_coordinates.append(dat)
+
+    return new_board_coordinates
+
+    
+##TARGETS TO USE:
+#Row 1 : 3 66 6 53 9 63 13
+#Row 2 : 50 157 60 159 57 161 55
+#Row 3 : 2 49 5 51 8 65 12
+#Row 4 : 56 160 52 158 64 145 61
+#Row 5 : 1 58 4 59 7 54 11
+
+board_targets=["target 3","target 66","target 6","target 53","target 9","target 63","target 13",
+"target 50","target 157","target 60","target 159","target 57","target 161","target 55",
+"target 2","target 49","target 5","target 51","target 8","target 65","target 12",
+"target 56","target 160","target 52","target 158","target 64","target 145","target 61",
+"target 1","target 58","target 4","target 59","target 7","target 54","target 11"]
+
+board_targets.sort(key=lambda x: int(x[7:len(x)]))
+
+
+
+####PRUEBA
+
+###GET TARGET DATA
+
+#PATH 4: TARGET_S
+type_="target"
+s4=open_files(type_,PATH_4)
+
+target_s_filter1=filter_information(type_,s4)
+
+target_s_filter2=targets_xyz_still(target_s_filter1)
+
+target_s_boardfilter=filter_points(type_,target_s_filter2,board_targets)
+
+target_s_xyz=only_coordinates(target_s_boardfilter)
+
+
+#PATH 2: TARGET_M
+type_="target"
+s2=open_files(type_,PATH_2)
+
+target_m_filter1=filter_information(type_,s2)
+
+target_m_filter2=targets_xyz_moving(target_m_filter1)
+
+target_m_boardfilter=filter_points(type_,target_m_filter2,board_targets)
+
+target_m_xyz=only_coordinates(target_m_boardfilter)
+
+###GET CAMERA DATA
+
+#PATH 1: CAMERA_M
+type_="camera"
+s1=open_files(type_,PATH_1)
+
+camera_m_filter1=filter_information(type_,s1)
+
+camera_m_xyz=camera_coordinates(camera_m_filter1)
+
+m_image_paths=image_paths("photos/moving photos",camera_m_xyz)
+
+
+#PATH 3: CAMERA_S
+type_="camera"
+s3=open_files(type_,PATH_3)
+
+camera_s_filter1=filter_information(type_,s3)
+
+camera_s_xyz=camera_coordinates(camera_s_filter1)
+
+s_image_paths=image_paths("photos/still photos",camera_s_xyz)
+
+cropped_image_paths=image_paths(PATH_7,camera_s_xyz)
+
+###GET PIXEL DATA
+
+#PATH 5: PIXELS_M
+type_="pixel"
+s5=open_files(type_,PATH_5)
+
+pixels_m_filter1=filter_information(type_,s5)
+
+pixels_m_xyz=pixel_coordinate(pixels_m_filter1)
+
+pixels_m_boardfilter=filter_points(type_,pixels_m_xyz,board_targets)
+
+
+#PATH 6: PIXELS_S
+type_="pixel"
+s6=open_files(type_,PATH_6)
+
+pixels_s_filter1=filter_information(type_,s6)
+
+pixels_s_xyz=pixel_coordinate(pixels_s_filter1)
+
+pixels_s_boardfilter=filter_points(type_,pixels_s_xyz,board_targets)
+
+
+
+####VARIABLES FOR EXPORTING
+
+#Targets used are given by and sorted in the same order as 'board_targets'.
+#Target coordinates are given in X,Y,Z order. List of tuples.
+#Cameras are given in FILENAME,X,Y,Z,YAW,PITCH,ROLL. List of lists.
+#pixels are given in FILENAME,TARGET NAME,X,Y order. List of lists.
+
+TARGET_S_COORDINATES=target_s_xyz
+TARGET_M_COORDINATES=target_s_xyz
+
+CAMERA_S_COORDINATES=camera_s_xyz
+CAMERA_M_COORDINATES=camera_m_xyz
+
+STILL_IMAGE_PATHS=s_image_paths
+MOVING_IMAGE_PATHS=m_image_paths
+CROPPED_IMAGE_PATHS=cropped_image_paths
+
+PIXELS_S_COORDINATES=pixels_s_boardfilter
+PIXELS_M_COORDINATES=pixels_m_boardfilter
+
+#OPEN CAMERA CALIBRATION XML FILE
+
+cv_file=cv.FileStorage("scripts/opencv_cam_calibration.xml",cv.FILE_STORAGE_READ)
+
+CAMERA_MATRIX=cv_file.getNode("Camera_Matrix").mat()
+DISTORTION_COEF=cv_file.getNode("Distortion_Coefficients").mat()
+print("read matrix\n",CAMERA_MATRIX)
+print("read matrix\n",DISTORTION_COEF)
+cv_file.release()
---
+++ scripts/projection_finder.py
@@ -0,0 +1,182 @@
+import cv2 as cv
+import numpy as np
+import create_lists as lists
+
+
+
+#CALIBRATE CAMERA, need more data
+class CalibrateCamera: 
+    def __init__(self,images,object_points):
+
+        self.image_points=[]
+        self.object_points=np.array(object_points)#list of coordinates
+        self.dist_coeff=[0]
+        self.images=images #list of images
+        self.boardSize=(7,5)
+        #define columns and rows
+        self.criteria = (cv.TERM_CRITERIA_EPS + cv.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        #define criteria
+
+
+    def circles_grid_centers(self):
+
+        #To consider: The parameters below depend on pixel distances, areas, etc. The images have to be similar to be able to use the same parameters. For different sets of images, different parameters are needed.
+        #For example: from folder "cropped_images" images sets are DSC09900 to DSC09915, DSC9916 to DSC09926, DSC09927 to DSC09934, etc.
+        #FROM CROPPED_IMAGES, 12 SUCCESSFUL CIRCLESGRID DETECTION FOR 35 CIRCLES.
+        blobParams = cv.SimpleBlobDetector_Params()
+        blobParams.filterByCircularity = True
+        blobParams.minCircularity = 0.2
+        blobParams.minDistBetweenBlobs = 130
+        blobParams.filterByInertia = True
+        blobParams.minInertiaRatio = 0.01
+        blobParams.filterByArea = True
+        blobParams.minArea = 1100
+        blobParams.maxArea = 1800
+        blobParams.minThreshold = 8.5
+        blobParams.maxThreshold = 2000
+        blobParams.filterByColor = True
+        blobParams.blobColor=0
+
+        self.blobdetector = cv.SimpleBlobDetector_create(blobParams)
+
+
+        for image in self.images:
+            img=cv.imread(image)
+            self.gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+            keypoints = self.blobdetector.detect(self.gray)
+            self.im_with_keypoints = cv.drawKeypoints(self.gray, keypoints, np.array([]), (0,255,0), cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+            #cv.imshow('img',self.im_with_keypoints)
+            #cv.waitKey(0)
+            im_with_keypoints_gray = cv.cvtColor(self.im_with_keypoints, cv.COLOR_BGR2GRAY)
+            self.retval,self.centers=cv.findCirclesGrid(self.im_with_keypoints, self.boardSize, flags=(cv.CALIB_CB_SYMMETRIC_GRID+cv.CALIB_CB_CLUSTERING),blobDetector=self.blobdetector,parameters=None)
+            if self.retval == True: 
+              #cornerSubPix(image, corners, winSize, zeroZone, criteria) -> corners
+                self.centers2=cv.cornerSubPix(self.gray, self.centers, (11,11),(-1,-1), self.criteria) 
+
+                self.image_points.append([image,self.centers2])
+    
+ 
+        # Draw and display the corners
+
+                img = cv.drawChessboardCorners(img, self.boardSize, self.centers2, self.retval)
+
+
+
+                #cv.imshow('img',img)
+                #cv.waitKey(0)
+
+    
+
+
+#CANNOT FIGURE OUT OBJECT POINTS AND IMAGE POINTS TYPE TO MAKE IT WORK :()
+    def calibrate(self):
+        #what is gray????? should this be for each picture???
+        self.retval_2,self.cam_matrix,self.dist_coeff,self.rotation_vector,self.translation_vector=cv.calibrateCamera(self.object_points, self.image_points, self.gray.shape[::-1], None, None)
+       # img = cv.imread('newimage') #what do i put in here?
+       # h,  w = img.shape[:2]
+        print(self.retval_2)
+        print(self.cam_matrix)
+        print(self.dist_coeff)
+        print(self.rotation_vector)
+        print(self.translation_vector)
+       # self.newcam_mtx, roi=cv.getOptimalNewCameraMatrix(self.cam_matrix,self.dist_coeff,(w,h),1,(w,h))
+
+
+class ProjectPoints:
+
+    def __init__(self, images, obj_coordinates_3d, camera_coordinates_3d, cam_matrix,dist_coef):
+
+        self.obj_coordinates=np.array(obj_coordinates_3d)
+        self.images=images
+        self.camera_coordinates=camera_coordinates_3d
+        self.camera_matrix=cam_matrix
+        self.distortion_coef=dist_coef
+        self.projection_pixels=[]
+
+
+    def projections(self,filename,rotation_vector,translation_vector):
+
+        pixels=cv.projectPoints(self.obj_coordinates,rotation_vector,translation_vector,self.camera_matrix,self.distortion_coef)
+
+        self.projection_pixels.append([filename,pixels[0]])
+
+
+
+    def create_projections(self):
+
+        for camera in self.camera_coordinates:
+            filename=camera[0]   
+            translation_vector=np.array(camera[1])
+            rotation_vector=np.array(camera[2])
+
+            self.projections(filename,rotation_vector,translation_vector)
+        
+
+
+
+def filter(points, images):
+    final=[]
+
+    for point in points:
+        for image in images:
+            if point[0]==image:
+                final.append(point)
+
+    return final
+
+if __name__ == '__main__':
+    #FINAL: PROJECT POINTS USING 3D TARGETS COORDINATES TAKEN FROM STILL IMAGES AND 3D CAMERA COORDINATES TAKEN FROM MOVING IMAGES.
+        #camera= from moving camera coordinates.
+        #object_points= from still target coordinates.
+        #camera_matrix= from still images.
+    print("START FINAL\n")
+    final_object_points= lists.TARGET_S_COORDINATES
+    final_images= lists.MOVING_IMAGE_PATHS
+    final_camera=lists.CAMERA_M_COORDINATES
+    final_camera_matrix=lists.CAMERA_MATRIX
+    final_distortion_coefs=lists.DISTORTION_COEF
+    final_projectpoints=ProjectPoints(final_images,final_object_points,final_camera,final_camera_matrix,final_distortion_coefs)
+    final_projectpoints.create_projections()
+    print("IMAGE POINTS FROM FINAL_PROJECTPOINTS:\n")
+    #print(final_projectpoints.projection_pixels)
+    FINAL_REPROJECTION_PIX=final_projectpoints.projection_pixels
+    
+
+    #TEST:
+    # 1: FINDING PIXELS OF TARGETS IN 12 CROPPED IMAGES WITH CALIBRATECAMERA.
+    # 2: FINDING REPROJECTED PIXELS OF TARGETS WITH PROJECTPOINTS. 
+    # 3: COMPARING 1 AND 2 WITH ORIGINAL PIXELS OBTAINED FROM METASHAPE.
+
+        #camera=from still camera coordinates.
+        #object_points= from still target coordinates.
+        #camera_matrix= from still images.
+
+    print("START TEST\n")
+    test_object_points= lists.TARGET_S_COORDINATES
+    test_images= lists.CROPPED_IMAGE_PATHS
+    test_camera=lists.CAMERA_S_COORDINATES
+    test_camera_matrix=lists.CAMERA_MATRIX
+    test_distortion_coefs=lists.DISTORTION_COEF
+
+    test_calibrate=CalibrateCamera(test_images,test_object_points)
+    test_calibrate.circles_grid_centers()
+    print("IMAGE POINTS FROM TEST_CAMERACALIBRATION:\n")
+    #print(test_calibrate.image_points)
+    #print("\n")
+    TEST_CALIBRATION_PIX=test_calibrate.image_points
+    test_projectpoints=ProjectPoints(test_images,test_object_points,test_camera,test_camera_matrix,test_distortion_coefs)
+    test_projectpoints.create_projections()
+    print("IMAGE POINTS FROM TEST_PROJECTPOINTS:\n")
+    #print(projectpoints.projection_pixels)
+    test_images_paths=['DSC09901.JPG','DSC09902.JPG','DSC09905.JPG','DSC09906.JPG','DSC09908.JPG','DSC09909.JPG','DSC09910.JPG','DSC09911.JPG','DSC09912.JPG','DSC09913.JPG','DSC09914.JPG','DSC09939.JPG']
+    test_filter_reprojections=filter(test_projectpoints.projection_pixels,test_images_paths)
+    #print(test_filter_reprojections)
+    TEST_REPROJECTION_PIX=test_filter_reprojections
+
+
+
+
+
+
+#https://python.hotexamples.com/examples/cv2/-/projectPoints/python-projectpoints-function-examples.html
+
